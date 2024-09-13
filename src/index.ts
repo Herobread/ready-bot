a6f0023ecacd9e9c46e481d2eae7756f78f3dcee
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-09-13 11:25:38 +0100

    add node tg bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("testjdjdskjsjsssaaaa");
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.TOKEN;
+
+console.log(token);
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg: any) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "Received your message");
+});
