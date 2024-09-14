34890cde0cfbb9183f8ec64ae28732d867a99376
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-09-14 10:35:09 +0100

    basic functionality

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
-const TelegramBot = require("node-telegram-bot-api");
+import TelegramBot, { CallbackQuery, Message } from "node-telegram-bot-api";
 
 const token = process.env.TOKEN;
 
-console.log(token);
+if (!token) {
+  throw new Error('Bot token is not defined')
+}
 
-const bot = new TelegramBot(token, { polling: true });
+const bot:TelegramBot = new TelegramBot(token, { polling: true });
 
-bot.on("message", (msg: any) => {
+const READY_PRESSED_CALLBACK = 'READY_PRESSED_CALLBACK'
+
+bot.onText(/\/go/, (msg: Message) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, "Received your message");
+  const statusMessage = bot.sendMessage(chatId, 'les go', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {text:'Ready', callback_data: READY_PRESSED_CALLBACK}
+        ]
+      ]
+    }
+  })
 });
+
+bot.on('callback_query', (callbackQuery:CallbackQuery) => {
+  const {message} = callbackQuery
+  if ( !message ) {
+    return
+  }
+  const chatId = message.chat.id
+  const messageId = message.message_id
+
+  if ( callbackQuery.data === READY_PRESSED_CALLBACK) {
+    bot.editMessageText('pressed ready', {
+      chat_id: chatId,
+      message_id: messageId,
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: 'Ready', callback_data: READY_PRESSED_CALLBACK }
+          ]
+        ]
+      }
+    })
+  }
+})
